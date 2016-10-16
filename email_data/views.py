637d364ca9f547995ef470fc2de6213392251e34
arpitj938 <arpitj938@gmail.com>
2016-10-16 18:50:41 +0530

    custom subject and message done

--- email_data/views.py
+++ email_data/views.py
@@ -9,35 +9,45 @@ class UploadFileForm(forms.Form):
     file = forms.FileField()
 
 def send_mass_mails(request):
-	try:
-		group_id=request.POST.get("group_id")
-		subject=str(request.POST.get("subject"))
-		message=str(request.POST.get("message"))
-		#group_id=1;
-		#subject="Hello"
-		#message="For testing purpose"
-		response={}
-		for o in user_data.objects.filter(group_id=group_id):
-			send_mail(
-				subject,
-				message,
-				'no-reply@aditya.com',
-				[str(o.user_emailid)],
-				fail_silently=False,
-				)
-		response["success"]=True
-		response["message"]="email sent"
-	except:
-		response["success"]=False
-		response["message"]="email not sent"
-
-	print str(response)
-	return HttpResponse(str(response))
+	if request.method=="POST":
+	    try:
+	    	group_id=request.POST.get("group_id")
+	    	print group_id
+	    	subject=str(request.POST.get("subject"))
+	    	print subject
+	    	message=str(request.POST.get("message"))
+	    	print message
+	    	#group_id=1;
+	    	#subject="Hello"
+	    	#message="For testing purpose"
+	    	response={}
+	    	for o in user_data.objects.filter(group_id=group_id):
+	    		send_mail(
+	    			subject,
+	    			message,
+	    			'no-reply@aditya.com',
+	    			[str(o.user_emailid)],
+	    			fail_silently=False,
+	    			)
+	    	response["success"]=True
+	    	response["message"]="email sent"
+	    except:
+	    	response["success"]=False
+	    	response["message"]="email not sent"
+    
+	    print str(response)
+	    return HttpResponse(str(response))
+	else:
+		return render(
+            request,
+            'send.html',
+            {'title':'send email'},{'header': 'write subject message'})
 
 def import_sheet(request):
     if request.method == "POST":
         form = UploadFileForm(request.POST,
                               request.FILES)
+        
         if form.is_valid():
             request.FILES['file'].save_to_database(
                 model=user_data,
@@ -51,6 +61,7 @@ def import_sheet(request):
         request,
         'upload.html',
         {'form': form})
+
 
 	
 
